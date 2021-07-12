df2dcc753d09952a1f1e7ab26a379f38a19cbeba
caiqiyuan <cqyisbug@163.com>
2021-07-12 17:01:10 +0800

    添加个别方法的注释

--- factory-bean/src/main/java/cn/cqy/learn/factorybean/CustomInvocationHandler.java
+++ factory-bean/src/main/java/cn/cqy/learn/factorybean/CustomInvocationHandler.java
@@ -9,6 +9,9 @@ import java.lang.reflect.Method;
  * @author caiqy
  */
 public class CustomInvocationHandler implements InvocationHandler {
+
+    // 想了一下feign的实现,基本上就和这个差不太多了,通过method获取到返回类型,然后利用注解获取到的信息创建一个
+    // http请求,就是如此的简单
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         String msg = StringUtils.arrayToCommaDelimitedString(args);
